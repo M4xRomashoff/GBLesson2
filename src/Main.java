b958759cb5b6a5c6770cd8a14fae0a207205f306
Max Romashoff <m4xromashoff@yandex.ru>
2024-06-01 17:06:39 +0300

    + 1 human

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,14 @@ public class Main {
         Market market = new Market();
         Human human1 = new Human("Ivan");
         Human human2 = new Human("Vladimir");
+        Human human3 = new Human("Наталья");
         market.acceptToMarket(human1);
         market.acceptToMarket(human2);
+        market.acceptToMarket(human3);
+
+        market.takeInQueue(human1);
+
+
         market.update();
     }
 }
